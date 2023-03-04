d0953c394475e92e0131e3908837bd961e32cd9f
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-03-04 15:56:27 +0100

    fix: ensure that updated item exists before updating (#30)

---
+++ MiniSim/Extensions/Collection+get.swift
@@ -0,0 +1,19 @@
+//
+//  Collection+get.swift
+//  MiniSim
+//
+//  Created by Oskar Kwaśniewski on 04/03/2023.
+//
+
+import Foundation
+
+extension Collection {
+
+    /// Get at index object
+    ///
+    /// - Parameter index: Index of object
+    /// - Returns: Element at index or nil
+    func get(at index: Index) -> Iterator.Element? {
+        return self.indices.contains(index) ? self[index] : nil
+    }
+}
