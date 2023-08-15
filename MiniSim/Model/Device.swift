bb1dcbac6e151fc9ce1b5b7fe8ac2268c095b3bb
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-08-15 23:53:48 +0200

    feat: implement custom coding keys to support AppleScript

--- MiniSim/Model/Device.swift
+++ MiniSim/Model/Device.swift
@@ -24,4 +24,35 @@ struct Device: Hashable, Codable {
             return name
         }
     }
+    
+    enum CodingKeys: String, CodingKey {
+        case name, version, ID, booted, platform, displayName
+    }
+    
+    init(name: String, version: String? = nil, ID: String?, booted: Bool = false, platform: Platform) {
+        self.name = name
+        self.version = version
+        self.ID = ID
+        self.booted = booted
+        self.platform = platform
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        version = try values.decode(String.self, forKey: .version)
+        ID = try values.decode(String.self, forKey: .ID)
+        booted = try values.decode(Bool.self, forKey: .booted)
+        platform = try values.decode(Platform.self, forKey: .platform)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(version, forKey: .version)
+        try container.encode(ID, forKey: .ID)
+        try container.encode(booted, forKey: .booted)
+        try container.encode(platform, forKey: .platform)
+        try container.encode(displayName, forKey: .displayName)
+    }
 }
