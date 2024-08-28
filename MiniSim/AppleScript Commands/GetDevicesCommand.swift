e9caf240cb74628488bee0080645cb72356597db
Rami Elwan <ramielwan48@gmail.com>
2024-08-28 18:44:39 +0200

    feat: make getDevices command return devices according to device type

--- MiniSim/AppleScript Commands/GetDevicesCommand.swift
+++ MiniSim/AppleScript Commands/GetDevicesCommand.swift
@@ -11,19 +11,25 @@ import Foundation
 class GetDevicesCommand: NSScriptCommand {
     override func performDefaultImplementation() -> Any? {
         guard
-            let argument = self.property(forKey: "platform") as? String,
-            let platform = Platform(rawValue: argument)
+            let platformArg = self.property(forKey: "platform") as? String,
+            let platform = Platform(rawValue: platformArg),
+            let deviceTypeArg = self.property(forKey: "deviceType") as? String,
+            let deviceType = DeviceType(rawValue: deviceTypeArg)
         else {
             scriptErrorNumber = NSRequiredArgumentsMissingScriptError
             return nil
         }
 
         do {
-            switch platform {
-            case .android:
-                return try self.encode(DeviceService.getAndroidDevices())
-            case .ios:
-                return try self.encode(DeviceService.getIOSDevices())
+            switch (platform, deviceType) {
+            case (.android, .physical):
+                return try self.encode(DeviceService.getAndroidPhysicalDevices())
+            case (.android, .virtual):
+                return try self.encode(DeviceService.getAndroidEmulators())
+            case (.ios, .physical):
+                return try self.encode(DeviceService.getIOSPhysicalDevices())
+            case (.ios, .virtual):
+                return try self.encode(DeviceService.getIOSSimulators())
             }
         } catch {
             scriptErrorNumber = NSInternalScriptError
